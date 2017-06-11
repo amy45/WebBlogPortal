4e6196fd0d6056d04a10be9675b7b959651bbfd8
Amit Jethwani <jethwaniamy45@gmail.com>
2017-06-11 20:40:45 +0530

    Test Branch

--- src/main/resources/static/js/controller/my_controller2.js
+++ src/main/resources/static/js/controller/my_controller2.js
@@ -3,5 +3,18 @@
 define(function () {
     return ['$scope', 'VersionService', function ($scope, versionService) {
 
+    	
+    	
+    	$scope.data=[{"img":"images/1.jpg"},
+    	             {"img":"images/2.jpg"},
+    	             {"img":"images/3.jpg"},
+    	             {"img":"images/4.jpg"},
+    	             {"img":"images/5.jpg"},
+    	             {"img":"images/6.jpg"},
+    	             {"img":"images/7.jpg"},
+    	             {"img":"images/8.jpg"},
+    	             ];
+    	
+    	$scope.check="Testing";
     }];
 });
